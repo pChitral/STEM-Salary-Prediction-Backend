262db3835ca2bd772f09f17cc8746a0acecbb9cb
pChitral <chitral.patil@vit.edu.in>
2023-05-06 20:48:30 -0400

    City Education Race CRUD up and working

--- main.py
+++ main.py
@@ -69,3 +69,108 @@ async def delete_race(item_id: int):
     conn.commit()
     conn.close()
     return {"item_id": item_id}
+
+
+# Education CRUD
+
+class EducationItem(BaseModel):
+    edu_id: int
+    edu_name: str
+
+
+@app.post("/education/create")
+async def create_education(item: EducationItem):
+    conn = sqlite3.connect('jobs_database.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO Education (edu_id, edu_name) VALUES (?, ?)",
+              (item.edu_id, item.edu_name))
+    conn.commit()
+    conn.close()
+    return {"item": item}
+
+
+@app.get("/educations/")
+async def read_educations():
+    conn = sqlite3.connect('jobs_database.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM Education")
+    items = []
+    for row in c.fetchall():
+        item = {"edu_id": row[0],  "edu_name": row[1]}
+        items.append(item)
+    conn.commit()
+    conn.close()
+    return {"items": items}
+
+
+@app.put("/education/update/{item_id}")
+async def update_education(item_id: int, item: EducationItem):
+    conn = sqlite3.connect('jobs_database.db')
+    c = conn.cursor()
+    c.execute("UPDATE Education SET edu_name = ? WHERE edu_id = ?",
+              (item.edu_name, item_id))
+    conn.commit()
+    conn.close()
+    return {"item_id": item_id, "item": item}
+
+
+@app.delete("/education/delete/{item_id}")
+async def delete_education(item_id: int):
+    conn = sqlite3.connect('jobs_database.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM Education WHERE edu_id = ?", (item_id,))
+    conn.commit()
+    conn.close()
+    return {"item_id": item_id}
+
+# City CRUD
+
+class CityItem(BaseModel):
+    city_id: int
+    city_name: str
+
+
+@app.post("/city/create")
+async def create_city(city_item: CityItem):
+    conn = sqlite3.connect('jobs_database.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO City (city_id, city_name) VALUES (?, ?)",
+              (city_item.city_id, city_item.city_name))
+    conn.commit()
+    conn.close()
+    return {"item": city_item}
+
+
+@app.get("/cities/")
+async def read_cities():
+    conn = sqlite3.connect('jobs_database.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM City")
+    items = []
+    for row in c.fetchall():
+        item = {"city_id": row[0],  "city_name": row[1]}
+        items.append(item)
+    conn.commit()
+    conn.close()
+    return {"items": items}
+
+
+@app.put("/city/update/{item_id}")
+async def update_city(item_id: int, city_item: CityItem):
+    conn = sqlite3.connect('jobs_database.db')
+    c = conn.cursor()
+    c.execute("UPDATE City SET city_name = ? WHERE city_id = ?",
+              (city_item.city_name, item_id))
+    conn.commit()
+    conn.close()
+    return {"item_id": item_id, "item": city_item}
+
+
+@app.delete("/city/delete/{item_id}")
+async def delete_city(item_id: int):
+    conn = sqlite3.connect('jobs_database.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM City WHERE city_id = ?", (item_id,))
+    conn.commit()
+    conn.close()
+    return {"item_id": item_id}
